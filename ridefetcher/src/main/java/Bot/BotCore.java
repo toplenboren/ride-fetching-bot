101e3e3919829ffbee5e9d975fbc5a968ea75f1f
toplenboren <toplenboren@yandex.ru>
2019-10-27 20:00:55 +0500

    Base structure Init

---
+++ ridefetcher/src/main/java/Bot/BotCore.java
@@ -0,0 +1,61 @@
+package Bot;
+
+import UserState.UserState;
+import UserState.UserStateManager;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class BotCore extends TelegramLongPollingBot {
+
+    private UserStateManager userStates = new UserStateManager();
+
+    public static void main(String[] args) {
+
+        System.getProperties().put("proxySet", "true");
+        System.getProperties().put("socksProxyHost", "127.0.0.1");
+        System.getProperties().put("socksProxyPort", "9150");
+
+        ApiContextInitializer.init();
+
+        TelegramBotsApi botapi = new TelegramBotsApi();
+        try {
+            botapi.registerBot(new BotCore());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return LocalSettings.botName;
+    }
+
+    @Override
+    public void onUpdateReceived(Update e) {
+        Message msg = e.getMessage();
+        String txt = msg.getText();
+        UserState user = userStates.findUserState(msg.getChatId());
+        sendMsg(msg, BotLogic.processCommand(txt, user));
+    }
+
+    @Override
+    public String getBotToken() {
+        return LocalSettings.botToken;
+    }
+
+    private void sendMsg(Message msg, String text) {
+        SendMessage s = new SendMessage();
+        s.setChatId(msg.getChatId());
+        s.setText(text);
+        try {
+            execute(s);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ ridefetcher/src/main/java/TramFetching/TramFetching.java
@@ -0,0 +1,8 @@
+package TramFetching;
+
+public class TramFetching {
+    public static int getTram() {
+        //NOQA, Not implemented
+        return 5;
+    }
+}
