65fce957c379ab6c14505b1627ff5558f4ee8973
VysokovskikhDanil <danil.ekb@gmail.com>
2019-10-31 15:22:51 +0500

    Make TramFetching script on kotlin

---
+++ ridefetcher/src/main/java/TramFetching/TramFetching.kts
@@ -0,0 +1,39 @@
+package TramFetching;
+
+import java.net.URL;
+import java.util.regex.Pattern;
+import java.io.File;
+import kotlin.collections.*;
+import kotlin.Array;
+
+
+
+fun GetHTML(url: String): String {
+    val urlConnection = URL(url).openConnection();
+    val content = urlConnection.getInputStream().bufferedReader().readText();
+    return content;
+}
+
+fun GetTramArrivalTime(tramNumer: Int): String {
+    val html = GetHTML("http://m.ettu.ru/station/3417");
+    val regex = Regex(pattern = """<b>.+</b>""");
+    val reg = Regex(pattern = """\d+\hмин""");
+    val a = reg.findAll(html);
+    val b = (regex.findAll(html));
+    val tramNumbers = (b.map { it.value }).toList();
+    val tramTimes = (a.map { it.value }).toList();
+    var tramArrival: String;
+    val indexOfTram = tramNumbers.indexOf("<b>"+tramNumer+"</b>");
+    if (tramTimes.size == 0) {
+        tramArrival = "Видимо не стоит ждать трамвая.";
+    } else if (indexOfTram > -1) {
+        tramArrival = tramTimes[indexOfTram - 1];
+    } else {
+        tramArrival = "Трамвай приедет не раньше чем через " + tramTimes[tramTimes.lastIndex];
+    }
+
+    return tramArrival;
+
+}
+
+println(GetTramArrivalTime(7));
