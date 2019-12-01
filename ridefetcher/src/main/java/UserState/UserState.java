7ed9c2e728065b615f14197399f614454074c6e5
toplenboren <toplenboren@yandex.ru>
2019-12-02 03:17:10 +0500

    New properties in UserState class

--- ridefetcher/src/main/java/UserState/UserState.java
+++ ridefetcher/src/main/java/UserState/UserState.java
@@ -3,6 +3,9 @@ package UserState;
 public class UserState {
 
     private String state;
+    private String startPoint;
+    private String finishPoint;
+    private int[] tramRoutes;
 
     UserState(String state) {
         this.state = state;
@@ -11,8 +14,32 @@ public class UserState {
     void setState(String newState) {
         this.state = newState;
     }
-
     public String getState() {
         return this.state;
+    }
+
+    void setStartPoint(String startPoint) {
+        this.startPoint = startPoint;
+    }
+
+    public String getStartPoint() {
+        return this.startPoint;
+    }
+
+    void setFinishPoint(String finishPoint) {
+        this.finishPoint = finishPoint;
+    }
+
+    public String getFinishPoint() {
+        return this.finishPoint;
+    }
+
+
+    void setTramRoutes(int[] tramRoutes) {
+        this.tramRoutes = tramRoutes;
+    }
+
+    public String getReadableTramRoutes() {
+        return this.tramRoutes.toString();
     }
 }
