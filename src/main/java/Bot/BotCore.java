0384ee2d795ff13996706a2c1d9c1285480eea4d
toplenboren <toplenboren@yandex.ru>
2019-12-11 02:07:33 +0500

    Production env

--- src/main/java/Bot/BotCore.java
+++ src/main/java/Bot/BotCore.java
@@ -17,9 +17,10 @@ public class BotCore extends TelegramLongPollingBot {
 
     public static void main(String[] args) {
 
-        System.getProperties().put("proxySet", "true");
-        System.getProperties().put("socksProxyHost", "127.0.0.1");
-        System.getProperties().put("socksProxyPort", "9150");
+// Production env
+//        System.getProperties().put("proxySet", "true");
+//        System.getProperties().put("socksProxyHost", "127.0.0.1");
+//        System.getProperties().put("socksProxyPort", "9150");
 
         ApiContextInitializer.init();
 
