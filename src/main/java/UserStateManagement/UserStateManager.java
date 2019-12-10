747baf2de2c49e44391e9b645fbf2abb3ed57aee
toplenboren <toplenboren@yandex.ru>
2019-12-11 02:29:57 +0500

    jedis integration

--- src/main/java/UserStateManagement/UserStateManager.java
+++ src/main/java/UserStateManagement/UserStateManager.java
@@ -2,41 +2,49 @@ package UserStateManagement;
 
 
 import jodd.json.JsonParser;
+import redis.clients.jedis.Jedis;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class UserStateManager {
 
     private JsonParser jsonParser = new JsonParser();
+    private static Jedis jedis;
 
-    public static void updateUser(Long userId, UserState state) {
-        if (userExists(userId)) {
-            memory.replace(userId, state.serialize());
+    {
+        try {
+            jedis = getConnection();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
         }
     }
 
-    private static Map<Long, String> memory = new HashMap<Long, String>();
+    private static boolean userExists(Long userId) {
+        return jedis.exists(String.valueOf(userId));
+    }
+
+    private static Jedis getConnection() throws URISyntaxException {
+        URI redisURI = new URI(System.getenv("REDIS_URL"));
+        return new Jedis(redisURI);
+    }
+
+    //private static Map<Long, String> memory = new HashMap<Long, String>();
 
     private static void registerNewUser(Long userId) {
-        memory.put(userId, new UserState().serialize());
+        jedis.set(String.valueOf(userId), new UserState().serialize());
+    }
+
+    public static void updateUser(Long userId, UserState state) {
+        if (userExists(userId)) {
+            jedis.set(String.valueOf(userId), state.serialize());
+        }
     }
 
     public static UserState getOrRegisterUserState(Long userId) {
         if (!userExists(userId)) {
             registerNewUser(userId);
         }
-        return JsonParser.create().parse(memory.get(userId), UserState.class);
-    }
-
-    public UserState getUserState(Long userId) throws Exception {
-        if (userExists(userId)) {
-            return jsonParser.parse(memory.get(userId), UserState.class);
-        }
-        throw new Exception("Bad key");
-    }
-
-    private static boolean userExists(Long userId) {
-        return memory.containsKey(userId);
+        return JsonParser.create().parse(jedis.get(String.valueOf(userId)), UserState.class);
     }
 }
